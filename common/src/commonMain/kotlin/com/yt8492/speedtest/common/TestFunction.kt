e188cdfa2fa7156bcc459a1d9876c8815ebc76e7
yt8492 <tmymyut2000@gmail.com>
2020-09-26 02:04:25 +0900

    fix test function

--- common/src/commonMain/kotlin/com/yt8492/speedtest/common/TestFunction.kt
+++ common/src/commonMain/kotlin/com/yt8492/speedtest/common/TestFunction.kt
@@ -3,9 +3,11 @@ package com.yt8492.speedtest.common
 import kotlin.random.Random
 
 fun testFunction() {
+    println("start")
     val randomArray = IntArray(5000000) {
         Random.nextInt()
     }
     val compareCount = randomArray.mergeSort()
     println("Compare Count: $compareCount")
+    println("end")
 }
